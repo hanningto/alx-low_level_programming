d8c624164e948da140e02b2aa9dc36b97026221f
hannington <omondihannington12@gmail.com>
2023-02-16 12:24:44 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *main - prints on screen
+ *main - program to print on screen
  *Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
